e03a38261148d8ef5627c638f2fc4a30d4ada5f1
Uta Wunderlich <Uta.Wunderlich@googlemail.com>
2011-09-29 12:42:10 +0200

    add task fields only if storage service is available

--- com.uwusoft.timesheet/src/com/uwusoft/timesheet/preferences/TimesheetPreferencePage.java
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/preferences/TimesheetPreferencePage.java
@@ -1,5 +1,6 @@
 package com.uwusoft.timesheet.preferences;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,7 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
@@ -33,18 +35,20 @@ public class TimesheetPreferencePage extends FieldEditorPreferencePage
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new StringFieldEditor(TimesheetApp.WORKING_HOURS, "Weekly working hours:",
-				getFieldEditorParent()));
-		addField(new ComboFieldEditor(TimesheetApp.DEFAULT_TASK, "Default task:", getTaskArray("Primavera"), // TODO
-				getFieldEditorParent()));
-		addField(new ComboFieldEditor(TimesheetApp.DAILY_TASK, "Daily task:", getTaskArray("Primavera"), // TODO
-				getFieldEditorParent()));
-		addField(new StringFieldEditor(TimesheetApp.DAILY_TASK_TOTAL, "Daily task total:",
-				getFieldEditorParent()));
 		addField(new ComboFieldEditor(StorageService.PROPERTY, "Storage System:", getSystemArray(StorageService.SERVICE_ID, "storage"),
 				getFieldEditorParent()));
 		addField(new ComboFieldEditor(SubmissionService.PROPERTY, "Submission System:", getSystemArray(SubmissionService.SERVICE_ID, "submission"),
 				getFieldEditorParent())); // TODO add custom list editor
+		addField(new IntegerFieldEditor(TimesheetApp.WORKING_HOURS, "Weekly working hours:",
+				getFieldEditorParent()));
+		if (getTaskArray("Primavera").length > 0) {
+			addField(new ComboFieldEditor(TimesheetApp.DEFAULT_TASK, "Default task:", getTaskArray("Primavera"), // TODO
+					getFieldEditorParent()));
+			addField(new ComboFieldEditor(TimesheetApp.DAILY_TASK, "Daily task:", getTaskArray("Primavera"), // TODO
+					getFieldEditorParent()));
+			addField(new StringFieldEditor(TimesheetApp.DAILY_TASK_TOTAL, "Daily task total:",
+					getFieldEditorParent()));
+		}
 	}
 
 	private String[][] getSystemArray(String serviceId, String serviceName) {
@@ -68,15 +72,20 @@ public class TimesheetPreferencePage extends FieldEditorPreferencePage
 	}
 	
 	private String[][] getTaskArray(String name) {
-		StorageService storageService = new ExtensionManager<StorageService>(StorageService.SERVICE_ID)
-				.getService(getPreferenceStore().getString(StorageService.PROPERTY));
-		List<String> tasksList = storageService.getTasks().get(name);
-		String[][] tasksArray = new String[tasksList.size()][2];
-		String[] tasks = tasksList.toArray(new String[0]);
-		for (int row = 0; row < tasksArray.length; row++) {
-			tasksArray[row][0] = tasks[row];
-			tasksArray[row][1] = tasks[row];
+		try {
+			StorageService storageService = new ExtensionManager<StorageService>(StorageService.SERVICE_ID)
+					.getService(getPreferenceStore().getString(StorageService.PROPERTY));
+			List<String> tasksList = storageService.getTasks() == null ? new ArrayList<String>()
+					: storageService.getTasks().get(name);
+			String[][] tasksArray = new String[tasksList.size()][2];
+			String[] tasks = tasksList.toArray(new String[0]);
+			for (int row = 0; row < tasksArray.length; row++) {
+				tasksArray[row][0] = tasks[row];
+				tasksArray[row][1] = tasks[row];
+			}
+			return tasksArray;
+		} catch (ClassCastException e) {
+			return new String[0][2];
 		}
-		return tasksArray;
 	}
 }
