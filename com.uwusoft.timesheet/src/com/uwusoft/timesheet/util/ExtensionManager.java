b1f8c43bcd5a2520fae27113fa42afc03f74fffa
Uta Wunderlich <Uta.Wunderlich@googlemail.de>
2011-10-27 22:02:54 +0200

    added "singleton" for extension instantiation

--- com.uwusoft.timesheet/src/com/uwusoft/timesheet/util/ExtensionManager.java
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/util/ExtensionManager.java
@@ -1,8 +1,6 @@
 package com.uwusoft.timesheet.util;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
@@ -13,45 +11,31 @@ import com.uwusoft.timesheet.extensionpoint.SubmissionServiceImpl;
 
 public class ExtensionManager<T> {
 
-	private IConfigurationElement[] config;
-	private static Map<String, List<Object>> services = new HashMap<String, List<Object>>();
+	private String serviceId;
+	private static Map<String, Map<String, Object>> services = new HashMap<String, Map<String, Object>>();
 
 	/**
 	 * @param serviceId
 	 */
 	public ExtensionManager(String serviceId) {
-		config = Platform.getExtensionRegistry().getConfigurationElementsFor(serviceId);
-	}
-	
-	public static List<Object> getServices(String serviceId, String contributorName) {
-		if (services.get(serviceId) == null) {
-			services.put(serviceId, new ArrayList<Object>());
-			for (IConfigurationElement e : Platform.getExtensionRegistry().getConfigurationElementsFor(serviceId)) {
-				if (e.getContributor().getName().equals(contributorName))
-					try {
-						services.get(serviceId).add(e.createExecutableExtension("class"));
-					} catch (CoreException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-			}
-		}
-		return services.get(serviceId);
+		this.serviceId = serviceId;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public T getService(String contributorName) {
-		try {
-			for (IConfigurationElement e : config) {
-				//System.out.println("Evaluating extension " + e.getContributor().getName());
+		if (services.get(serviceId) == null) {
+			services.put(serviceId, new HashMap<String, Object>());
+			for (IConfigurationElement e : Platform.getExtensionRegistry().getConfigurationElementsFor(serviceId)) {
 				if (e.getContributor().getName().equals(contributorName))
-					return (T) e.createExecutableExtension("class");
+					try {
+						services.get(serviceId).put(contributorName, (T) e.createExecutableExtension("class"));
+					} catch (CoreException e1) {
+						MessageBox.setError(this.getClass().getSimpleName(), e1.getLocalizedMessage());
+					}
 			}
-		} catch (ClassCastException ex) {
-            ex.printStackTrace(); // TODO
-		} catch (CoreException ex) {
-            ex.printStackTrace(); // TODO
+			if (services.get(serviceId).get(contributorName) == null)
+				services.get(serviceId).put(contributorName, new SubmissionServiceImpl()); // TODO submission service plug-in 
 		}
-		return (T) new SubmissionServiceImpl(); // TODO submission service plug-in
+		return (T) services.get(serviceId).get(contributorName);
 	}
 }
