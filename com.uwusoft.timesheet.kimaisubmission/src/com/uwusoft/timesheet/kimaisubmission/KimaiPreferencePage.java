084333c1d106e5cedc587a4bea4e0ada3c35d767
Uta Wunderlich <Uta.Wunderlich@googlemail.com>
2011-12-06 11:47:50 +0100

    added Kimai preferences and prepared login

---
+++ com.uwusoft.timesheet.kimaisubmission/src/com/uwusoft/timesheet/kimaisubmission/KimaiPreferencePage.java
@@ -0,0 +1,30 @@
+package com.uwusoft.timesheet.kimaisubmission;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.uwusoft.timesheet.Activator;
+
+public class KimaiPreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	public KimaiPreferencePage() {
+		super(GRID);
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Kimai preferences");
+		getPreferenceStore().addPropertyChangeListener(this);
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new StringFieldEditor(KimaiSubmissionService.USERNAME, "User name:",
+				getFieldEditorParent()));
+	}
+
+}
--- com.uwusoft.timesheet.kimaisubmission/src/com/uwusoft/timesheet/kimaisubmission/KimaiSubmissionService.java
+++ com.uwusoft.timesheet.kimaisubmission/src/com/uwusoft/timesheet/kimaisubmission/KimaiSubmissionService.java
@@ -5,24 +5,65 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.widgets.Display;
+
+import com.uwusoft.timesheet.Activator;
+import com.uwusoft.timesheet.dialog.LoginDialog;
 import com.uwusoft.timesheet.extensionpoint.SubmissionService;
+import com.uwusoft.timesheet.util.SecurePreferencesManager;
 
 public class KimaiSubmissionService implements SubmissionService {
 
+	public static final String USERNAME = "kimai.user.name";
+	public static final String PASSWORD = "kimai.user.password";
+
+    static {
+       	while (!authenticate());
+    }
+    
+	private static boolean authenticate() {
+		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+		SecurePreferencesManager secureProps = new SecurePreferencesManager("Kimai");
+		String userName = preferenceStore.getString(USERNAME);
+		String password = secureProps.getProperty(PASSWORD);
+		if (!StringUtils.isEmpty(userName) && !StringUtils.isEmpty(password)) {
+			// service.setUserCredentials(userName, password);
+			return true;
+		}
+
+		Display display = Display.getDefault();
+		LoginDialog loginDialog = new LoginDialog(display, "Kimai Log in",	"", userName, password);
+		if (loginDialog.open() == Dialog.OK) {
+			// service.setUserCredentials(loginDialog.getUser(), loginDialog.getPassword());
+			preferenceStore.setValue(USERNAME, loginDialog.getUser());
+			if (loginDialog.isStorePassword())
+				secureProps.storeProperty(PASSWORD, loginDialog.getPassword());
+			else
+				secureProps.removeProperty(PASSWORD);
+			return true;
+		}
+		Display.getDefault().dispose();
+		System.exit(1);
+		return false;
+	}
+
 	@Override
 	public List<String> getAssignedTasks() {
-        //Todo implement
-    	List<String> assignedTasks = new ArrayList<String>();
-    	assignedTasks.add("Sample Task 1");
-    	assignedTasks.add("Sample Task 2");
-    	assignedTasks.add("Sample Task 3");
-    	return assignedTasks;
+		// Todo implement
+		List<String> assignedTasks = new ArrayList<String>();
+		assignedTasks.add("Sample Task 1");
+		assignedTasks.add("Sample Task 2");
+		assignedTasks.add("Sample Task 3");
+		return assignedTasks;
 	}
 
 	@Override
 	public void submit(Date date, String task, Double total) {
-        //Todo implement
-        System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(date) + "\t" + task + "\t" + total);
+		// Todo implement
+		System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(date) + "\t" + task + "\t" + total);
 	}
 
 }
