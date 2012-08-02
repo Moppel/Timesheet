c205797b22614916bf9fa407cffb42ae51b6eb4d
Uta Wunderlich <Uta.Wunderlich@googlemail.com>
2012-08-02 08:36:29 +0200

    added column and label provider for project tasks

--- com.uwusoft.timesheet/src/com/uwusoft/timesheet/wizard/TaskListPage.java
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/wizard/TaskListPage.java
@@ -5,12 +5,16 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import com.uwusoft.timesheet.extensionpoint.model.SubmissionEntry;
 
@@ -31,16 +35,27 @@ public class TaskListPage extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setWidth(300);
+		column.setResizable(true);
+		viewerColumn.setLabelProvider(new ColumnLabelProvider() {
+            public String getText(Object element) {
+		        return ((SubmissionEntry) element).getName();
+			}
+			public Image getImage(Object obj) {
+				return AbstractUIPlugin.imageDescriptorFromPlugin("com.uwusoft.timesheet", "/icons/task_16.png").createImage();
+			}
+		});
         viewer.setContentProvider(ArrayContentProvider.getInstance());
-        viewer.setLabelProvider(new LabelProvider());
         viewer.setInput(tasks);
 		// Required to avoid an error in the system
 		setControl(parent);
 		setPageComplete(true);
 	}
 
+	@SuppressWarnings("unchecked")
 	public void addTasksToProjects(Map<String, Set<SubmissionEntry>> projects) {
-		projects.put(project, tasks); // TODO
-		//projects.put(project, new HashSet<SubmissionTask>(((StructuredSelection) viewer.getSelection()).toList()));
+		projects.put(project, new HashSet<SubmissionEntry>(((StructuredSelection) viewer.getSelection()).toList()));
 	}
 }
