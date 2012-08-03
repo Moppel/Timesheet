08e328ef44bc7f660d5034b6fb3c5d8d636f8a5e
Uta Wunderlich <Uta.Wunderlich@googlemail.com>
2012-08-03 12:37:14 +0200

    added import from all selected submission systems

--- com.uwusoft.timesheet/src/com/uwusoft/timesheet/commands/ImportTasksHandler.java
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/commands/ImportTasksHandler.java
@@ -1,8 +1,16 @@
 package com.uwusoft.timesheet.commands;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
@@ -16,13 +24,26 @@ public class ImportTasksHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
-		String system;
-		String[] systems = preferenceStore.getString(SubmissionService.PROPERTY).split(SubmissionService.separator);
-		if (systems.length == 1) system = systems[0];
-		else return null; // TODO list dialog for available submission services
-		WizardDialog dialog = new WizardDialog(Display.getDefault().getActiveShell(), new ImportTaskWizard(system));
-		dialog.open();							
+		Collection<String> systems = Arrays.asList(preferenceStore.getString(SubmissionService.PROPERTY).split(SubmissionService.separator));
+		if (systems.size() == 0) {
+			// This should only be the first setup
+			Map<String, String> availableSystems = new HashMap<String, String>();
+			for (IConfigurationElement e : Platform.getExtensionRegistry().getConfigurationElementsFor(SubmissionService.SERVICE_ID)) {
+				String contributorName = e.getContributor().getName();
+				availableSystems.put(Character.toUpperCase(contributorName.toCharArray()[contributorName.lastIndexOf('.') + 1])
+						+ contributorName.substring(contributorName.lastIndexOf('.') + 2, contributorName.indexOf(SubmissionService.SERVICE_NAME)),
+						contributorName);
+			}
+			if (!availableSystems.isEmpty()) {
+				// TODO create dialog (with check boxes) to select available systems and set them up
+				systems = availableSystems.values();
+				preferenceStore.setValue(SubmissionService.PROPERTY, StringUtils.join(systems, SubmissionService.separator));
+			}
+		}
+		for (String system : systems) {
+			WizardDialog dialog = new WizardDialog(Display.getDefault().getActiveShell(), new ImportTaskWizard(system));
+			dialog.open();							
+		}
 		return null;
 	}
-
 }
