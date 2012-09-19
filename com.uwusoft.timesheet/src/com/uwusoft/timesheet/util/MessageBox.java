78809a1b10b08b86295ea2535d071892f9a5efb2
Uta Wunderlich <Uta.Wunderlich@googlemail.com>
2012-09-19 13:46:18 +0200

    made message box run in display thread

--- com.uwusoft.timesheet/src/com/uwusoft/timesheet/util/MessageBox.java
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/util/MessageBox.java
@@ -8,7 +8,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.ToolTip;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
 import com.uwusoft.timesheet.Activator;
@@ -21,41 +20,70 @@ public class MessageBox {
 	private static ToolTip getSystemTrayToolTip() {
 		if (toolTip != null) return toolTip;
 		try {
-			IWorkbench workbench = PlatformUI.getWorkbench();
-			shell = new Shell(workbench.getDisplay(), SWT.NO_TRIM | SWT.ON_TOP); // TODO Invalid thread access?
-			if (workbench.getDisplay().getSystemTray().getItemCount() > 0) {
-				ToolTip tip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_INFORMATION);
-				workbench.getDisplay().getSystemTray().getItem(0).setToolTip(tip);
-				return tip;
-			}
-			return null;
+			final Display display = PlatformUI.getWorkbench().getDisplay();
+			display.asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					shell = new Shell(display, SWT.NO_TRIM | SWT.ON_TOP);
+					if (display.getSystemTray().getItemCount() > 0) {
+						toolTip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_INFORMATION);
+						display.getSystemTray().getItem(0).setToolTip(toolTip);
+					}
+				}
+			});
+			return toolTip;
 		} catch (IllegalStateException e) {
-			shell = new Shell(Display.getDefault(), SWT.NO_TRIM | SWT.ON_TOP);
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					shell = new Shell(Display.getDefault(), SWT.NO_TRIM | SWT.ON_TOP);
+				}
+			});
 			return null;
 		}
 	}
 
-	public static void setMessage(String title, String message) {
+	public static void setMessage(final String title, final String message) {
 		if ((toolTip = getSystemTrayToolTip()) == null) {
-			MessageDialog.openInformation(shell, title, message);
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					MessageDialog.openInformation(shell, title, message);
+				}
+			});
 		}
 		else {
-			toolTip.setText(title); 
-			toolTip.setMessage(message); 
-			toolTip.setVisible(true); 
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					toolTip.setText(title); 
+					toolTip.setMessage(message); 
+					toolTip.setVisible(true);
+				}
+			});
 		}
         logger.log(new Status(IStatus.INFO, Activator.PLUGIN_ID, title + ": " + message));
 	}
 	
-	public static void setError(String title, String message) {
+	public static void setError(final String title, final String message) {
 		if ((toolTip = getSystemTrayToolTip()) == null) {
-			MessageDialog.openError(shell, title, message);
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					MessageDialog.openError(shell, title, message);
+				}
+			});
 		}
 		else {
-			toolTip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_ERROR);
-			toolTip.setText(title); 
-			toolTip.setMessage(message == null ? "" : message); 
-			toolTip.setVisible(true); 
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					toolTip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_ERROR);
+					toolTip.setText(title); 
+					toolTip.setMessage(message == null ? "" : message); 
+					toolTip.setVisible(true); 
+				}				
+			});
 		}
         logger.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, title + ": " + message));
 	}
