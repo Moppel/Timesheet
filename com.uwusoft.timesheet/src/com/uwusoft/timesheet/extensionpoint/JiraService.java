3ae9795037483a813bfcbbd97e7ae8f2001a0b06
Uta Wunderlich <Uta.Wunderlich@googlemail.de>
2013-02-06 20:48:38 +0100

    added Jira extension point

---
+++ com.uwusoft.timesheet/src/com/uwusoft/timesheet/extensionpoint/JiraService.java
@@ -0,0 +1,7 @@
+package com.uwusoft.timesheet.extensionpoint;
+
+public interface JiraService {
+	public static final String SERVICE_ID = "com.uwusoft.timesheet.jiraservice";
+	public static final String SERVICE_NAME = "jira";
+
+}
